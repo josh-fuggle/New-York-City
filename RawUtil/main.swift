2187c573464be8a109745719dff3c9849b0852db
Joshua Fuglsang <joshua.fuglsang@gmail.com>
2015-06-20 10:32:02 +1000

    Initial Commit

---
+++ RawUtil/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RawUtil
+//
+//  Created by Joshua Fuglsang on 20/06/2015.
+//  Copyright (c) 2015 josh-fuggle. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
